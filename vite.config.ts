5fdb48e15c21375e38732ba37ea3b66324178a78
yinzixie <lhttnmb@gmail.com>
2022-10-15 17:41:28 +1100

    chore: install ant-design-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from "node:url"
-
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
+import Components from "unplugin-vue-components/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
