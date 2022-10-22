f88dba26207363d4228aedbefedc44e4ea7cbd25
yinzixie <lhttnmb@gmail.com>
2022-10-23 06:47:46 +1100

    fix: add test configs to support vscode vitest plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,19 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
 import Components from "unplugin-vue-components/vite"
+import type { UserConfig as VitestUserConfigInterface } from "vitest/config"
+
+/**
+ * Need setup test environment for vscode vitest plugin.
+ * https://github.com/kwai-explore/vscode-vitest-runner/issues/1
+ * Also need this extra const otherwise `defineConfig` will raise a type error.
+ * https://stackoverflow.com/questions/72146352/vitest-defineconfig-test-does-not-exist-in-type-userconfigexport/73106019#73106019
+ */
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    environment: "jsdom",
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,4 +31,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  test: vitestConfig.test,
 })
